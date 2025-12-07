ab9d01581a7f474e00f4ab1637974b7b178d3af8
annemorua <annettemoruac@gmail.com>
2025-12-07 15:35:40 -0600

    Update bank.py

--- Week1/Problems/bank.py
+++ Week1/Problems/bank.py
@@ -1,1 +1,11 @@
+greeting = input("Say a greeting: ").lower().strip(" ")
+#Just the word "hello".
+if greeting.startswith("hello"):
+    print("$0")
+#Any word that starst with h.
+elif greeting.startswith("h"):
+    print("$20")
+#Any other word that is not "hello" or begins with h.
+else:
+    print("$100")
 
