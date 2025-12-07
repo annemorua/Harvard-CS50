bf858b5d0d9a731c6bbe645c112d0895197c1cb8
annemorua <annettemoruac@gmail.com>
2025-12-07 15:33:17 -0600

    Update functions.py

--- Week0/functions.py
+++ Week0/functions.py
@@ -1,1 +1,15 @@
+def hello(to="world"):
+    print("Hello,", to)
+hello()
+name = input("What's your name? ")
+hello(name)
 
+#Creando un main.
+def main():
+    name2 = input("What's your name? ")
+    bye(name2)
+
+def bye(to="world"):
+    print("Bye,", to)
+
+main()
