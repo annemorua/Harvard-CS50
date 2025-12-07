4cd77fb91504c261daa96ad91d7b7007db8cc1c4
annemorua <annettemoruac@gmail.com>
2025-12-07 15:46:53 -0600

    Create or.py

---
+++ Week1/or.py
@@ -0,0 +1,25 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y or x > y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+
+#Lo mimso pero usando !=
+a = int(input("What's a? "))
+b = int(input("What's b? "))
+
+if a != b:
+    print("a is not equal to b")
+else:
+    print("a is equal to b")
+
+#Lo mimso pero usando ==
+e = int(input("What's e? "))
+f = int(input("What's f? "))
+
+if e == f:
+    print("e is equal to f")
+else:
+    print("e is not equal to f")
