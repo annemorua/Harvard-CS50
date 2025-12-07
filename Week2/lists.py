cb256f6c15dfdc21da30a3f5068bb668277d5bc5
annemorua <annettemoruac@gmail.com>
2025-12-07 15:50:27 -0600

    Create lists.py

---
+++ Week2/lists.py
@@ -0,0 +1,25 @@
+students = ["Hermione", "Harry", "Ron"]
+print(students[0])
+print(students[1])
+print(students[2])
+
+#Ahora sí con un for.
+
+students2 = ["Hermione", "Harry", "Ron"]
+for student in students2:
+    print(student)
+
+students3 = ["Hermione", "Harry", "Ron"]
+for i in range(len(students3)):
+    print(students3[i])
+
+students4 = ["Hermione", "Harry", "Ron"]
+for i in range(len(students4)):
+    print(i, students4[i])
+
+
+students5 = ["Hermione", "Harry", "Ron"]
+for i in range(len(students5)):
+    print(i + 1, students5[i])
+
+
