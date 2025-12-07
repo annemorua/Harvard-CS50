2d503add83dc6defe122b53bd8564f117663b8fb
annemorua <annettemoruac@gmail.com>
2025-12-07 15:50:56 -0600

    Create while.py

---
+++ Week2/while.py
@@ -0,0 +1,45 @@
+print("hola\n" * 3, end="")
+
+#Para diferenciar los whiles.
+print("")
+print("¡¡¡Cambio!!!")
+print("")
+
+i = 3
+while i != 0:
+    print("hello")
+    i = i - 1
+
+#Para diferenciar los whiles.
+print("")
+print("¡¡¡Cambio!!!")
+print("")
+
+i = 0
+while i < 3:
+    print("bonjour")
+    i = i + 1
+
+#Para diferenciar los whiles.
+print("")
+print("¡¡¡Cambio!!!")
+print("")
+
+i = 0
+while i < 3:
+    print("hallo")
+    i += 1
+
+#Para diferenciar los whiles.
+print("")
+print("¡¡¡Cambio!!!")
+print("")
+
+while True:
+    n = int(input("What's n? "))
+    if n < 0:
+        print("konnichiwa")
+    else:
+        break
+
+
