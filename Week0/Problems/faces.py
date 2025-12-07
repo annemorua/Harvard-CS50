fa74e6e6e9bdb2be91e8d9bb08065ff1660f36c8
annemorua <annettemoruac@gmail.com>
2025-12-07 15:31:44 -0600

    Update faces.py

--- Week0/Problems/faces.py
+++ Week0/Problems/faces.py
@@ -1,1 +1,15 @@
+def main():
+    emoji = input("Write a sentence: ")
+    #Calls the convert function.
+    sentence = convert(emoji)
+    print(sentence)
+
+def convert(emoticon):
+    #Replace all ":)" with smiley emoticons.
+    converted1 = emoticon.replace(":)", "🙂")
+    #One it changed all ":)", use converted1 and replace all ":(" with frown emoticons.
+    converted2 = converted1.replace(":(", "🙁")
+    return converted2
+
+main()
 
