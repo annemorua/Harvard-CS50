2a4a50c205180ecd2ee4a716e0e21f10857decad
annemorua <annettemoruac@gmail.com>
2025-12-07 15:47:50 -0600

    Update camel.py

--- Week2/Problems/camel.py
+++ Week2/Problems/camel.py
@@ -1,1 +1,9 @@
+camel = input("Write a sentence: ")
+
+for i in camel:
+    #If it finds an uppercase letter, it replace that letter with an "_" and the same letter but in lowercase.
+    if i.isupper():
+        camel = camel.replace(i, "_" + i.lower())
+print(camel)
+
 
