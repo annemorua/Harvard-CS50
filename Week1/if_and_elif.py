337babfa0b46b9534afa9b692675f8dd03725651
annemorua <annettemoruac@gmail.com>
2025-12-07 15:45:20 -0600

    Create if_and_elif.py

---
+++ Week1/if_and_elif.py
@@ -0,0 +1,25 @@
+#Also conditionals
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y:
+    print("x is less than y")
+
+if x > y:
+    print("x is greater than y")
+
+if x == y:
+    print("x is equal to y")
+
+#Ahora usando elif
+a = int(input("What's a? "))
+b = int(input("What's b? "))
+
+if a < b:
+    print("a is less than b")
+
+elif a > b:
+    print("a is greater than b")
+
+elif a == b:
+    print("a is equal to b")
