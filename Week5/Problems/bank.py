e247548842f4500acc08170a7828a955716bedcf
annemorua <annettemoruac@gmail.com>
2025-12-09 23:03:13 -0600

    Update bank.py

--- Week5/Problems/bank.py
+++ Week5/Problems/bank.py
@@ -1,1 +1,25 @@
+def main():
+    greeting = input("Say a greeting: ").strip(" ")
+
+    #Calls the "value" function and prints the amount of money.
+    print(value(greeting))
+
+def value(greeting):
+    #Makes the greeting lowercase.
+    greeting = greeting.lower()
+
+    #If the greeting starts with "hello", returns 0 dollars.
+    if greeting.startswith("hello"):
+        return "$0"
+
+    #If the greeting starts with "h", returns  20 dollars.
+    elif greeting.startswith("h"):
+        return "$20"
+
+    #Otherwise, returns 100 dollars.
+    else:
+        return "$100"
+
+if __name__ == "__main__":
+    main()
 
