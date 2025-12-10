a92743522163dbdb5357c10fea1d9146b303d4f1
annemorua <annettemoruac@gmail.com>
2025-12-09 23:36:38 -0600

    Create walrus_operator.py

---
+++ Week7/walrus_operator.py
@@ -0,0 +1,10 @@
+#todo walsrus :=
+
+import re
+
+name = input("What's your name? ").strip()
+
+if matches := re.search(r"^(.+), *(.+)$", name):
+    name = matches.group(2) + " " + matches.group(1)
+
+print(f"haello, {name}")
