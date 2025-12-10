d0e644f676ade9c3c4e3833724ad72d714809d86
annemorua <annettemoruac@gmail.com>
2025-12-09 23:58:47 -0600

    Create map.py

---
+++ Week9/map.py
@@ -0,0 +1,39 @@
+def main():
+    yell(["This", "is", "CS50"])
+
+def yell(words):
+    uppercased = []
+    for word in words:
+        uppercased.append(word.upper())
+    print(*uppercased)
+
+if __name__ == "__main__":
+    main()
+
+
+            ###Ahora con tuplas###
+
+def main():
+    yell("This", "is", "CS50")
+
+def yell(*words):
+    uppercased = []
+    for word in words:
+        uppercased.append(word.upper())
+    print(*uppercased)
+
+if __name__ == "__main__":
+    main()
+
+
+            ###Ahora con map###
+
+def main():
+    yell("This", "is", "CS50")
+
+def yell(*words):
+    uppercased = map(str.upper, words)
+    print(*uppercased)
+
+if __name__ == "__main__":
+    main()
