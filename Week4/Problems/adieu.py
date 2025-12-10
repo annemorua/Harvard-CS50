af0db9522c480193ff3055aef138f9d21cc30fbd
annemorua <annettemoruac@gmail.com>
2025-12-09 22:55:06 -0600

    Update adieu.py

--- Week4/Problems/adieu.py
+++ Week4/Problems/adieu.py
@@ -1,1 +1,24 @@
+import inflect
+
+#Starts an instanc with which you can do various formatting operations.
+stru = inflect.engine()
+
+#An empty list is started where the names entered by the user will be saved.
+names = []
+while True:
+    try:
+        name =  input("Name: ")
+
+        #Append the names to the "names" list.
+        names.append(name)
+
+        #Takes the names from the list, separates each of the names with a comma except the las two, which separates the with an "and".
+        #If there are only two names, separate the with "and".
+        comma = stru.join(names)
+
+    except EOFError:
+        break
+
+#Prints the phrase with the formatted names.
+print("Adieu, adieu, to " + comma)
 
