4e5036f7786356874952fd07398bfb599daa0071
annemorua <annettemoruac@gmail.com>
2025-12-09 22:57:32 -0600

    Create style.py

---
+++ Week4/style.py
@@ -0,0 +1,23 @@
+#Según pitchi bebé es como APA para python.
+#Es una forma de estandarizar los códigos para que sea más facil de leer.
+#Se puede ver en la dirección peps.python.org/pep-0008/.
+#Readability counts
+#--->Indentation
+#--->Tabs or spaces?
+#--->Maximum Line Lenght
+#--->Blank Lines
+#--->Imports
+#...                         #pycodestyle.pycqa.org
+
+#¡¡¡BLACK!!!
+#Se installa con pip install black            black.readthedocs.io
+
+#Para usarlo se pone "black name.py"
+
+students = {"Hermione":"Gryffindor",
+            "Harry":"Gryffindor",
+            "Ron":"Gryffindor",
+            "Draco":"Slytherin"}
+for student in students:
+    print(student)
+
