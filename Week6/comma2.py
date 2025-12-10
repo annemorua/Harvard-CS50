118627cd5a5181146e3f8565026c52ca8f54dfc1
annemorua <annettemoruac@gmail.com>
2025-12-09 23:17:34 -0600

    Create comma2.py

---
+++ Week6/comma2.py
@@ -0,0 +1,33 @@
+students = []
+
+with open("zstudents.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        students.append(f"{name} is in {house}")
+
+for student in sorted(students):
+    print(student)
+
+#Con un diccionario.
+
+with open("zstudents.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        student = {}
+        student["name"] = name
+        student["house"] = house
+        students.append(student)
+
+for student in students:
+    print(f"{student['name']} is in {student['house']}")
+
+#De forma más compacta.
+
+with open("zstudents.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        student = {"name": name, "house": house}
+        students.append(student)
+
+for student in students:
+    print(f"{student['name']} is in {student['house']}")
